4fb35a7cf765d4a86c61ed28e598b35b0503d798
Marcos Gabriel <marcosgabriel79355@gmail.com>
2022-08-30 09:51:34 -0300

    Criando insert ao banco de dados

--- pos-java-jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
+++ pos-java-jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
@@ -2,12 +2,20 @@ package pos_java_jdbc.pos_java_jdbc;
 
 import org.junit.Test;
 
-import conexaojdbc.SingleConnection;
+import Model.Userposjava;
+import dao.UserPosDAO;
 
 public class TesteBancoJdbc{
 
 	@Test
 	public void intBanco() {
-		SingleConnection.getConnection();
+		UserPosDAO userPosDAO = new UserPosDAO();
+		Userposjava userposjava = new Userposjava();
+		
+		userposjava.setId(5L);
+		userposjava.setNome("João");
+		userposjava.setEmail("joão@gmail.com");
+		
+		userPosDAO.salvar(userposjava);
 	}
 }
